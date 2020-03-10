2519af7c0d3a0a018ff58a3e89d0892a34b41338
bwakedavis <bwakedavis@gmail.com>
2020-03-10 18:14:15 +0300

    successfully fetch the recipes and display them on the dom

---
+++ build/precache-manifest.f4ea6c3c1ff0a02b75bbdbc3333823e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca8dec1f959d5157c2788c713f57a2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f3e913b4ee79532c169",
+    "url": "/static/css/main.6b0e5d9e.chunk.css"
+  },
+  {
+    "revision": "bccb0226269e6963845e",
+    "url": "/static/js/2.855a994d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.855a994d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3e913b4ee79532c169",
+    "url": "/static/js/main.f248b569.chunk.js"
+  },
+  {
+    "revision": "e06c96b9be6927f5c0c3",
+    "url": "/static/js/runtime-main.10bc7ff2.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -2,24 +2,45 @@
 
 import React,{useEffect, useState} from "react";
 import "./App.css";
+import Recipe from "./Recipe";
 
 function App() {
   const APP_ID = "e053ce49";
   const APP_KEY = "7104f7f24b96c2a57b7ade1afef060ff	";
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  const [counter, setCounter] = useState(0)
+ const [recipes, setRecipes] = useState([])
+ const [search, setSearch]=useState("")
+ const [query,setQuery] = useState('chicken')
   useEffect(()=>{
-    console.log('Effect runned')
-  })
-
+    // console.log('Effect runned')
+    getrecipes()
+  },[query])
+  const getrecipes = async ()=>{
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data =await response.json();
+    setRecipes(data.hits)
+    console.log(data.hits)
+  }
+  const updateSearch= e =>{
+    setSearch(e.target.value)
+    console.log(search)
+  }
+  const getSearch = e =>{
+    e.preventDefault();
+    setQuery(search)
+    setSearch('')
+  }
   return (
     <div className="App">
-      <form className="search-form">
-        <input type="text" className="search-bar"/>
-  <button type="submit"className="searh-button">Search</button>
+      <form className="search-form" onSubmit={getSearch} >
+        <input type="text" className="search-bar" value={search} onChange={updateSearch} />
+  <button type="submit" className="search-button">Search</button>
       </form>
-  <h1  onClick={()=> setCounter(counter +1)} >{counter}</h1>
+      <div className="recipes">
+    {recipes.map((recipe)=>(
+      <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients} />
+    ))}
+    </div>
     </div>
   );
 }
